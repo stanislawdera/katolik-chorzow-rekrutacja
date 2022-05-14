3f4797ab9e3b97dcc969718ba0c8334f6d7eaf18
Stanisław Dera <stanislawdera@outlook.com>
2022-05-14 20:25:15 +0200

    Initial changes

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Rekrutacja do LO - Katolik Chorzów</title>
+        <meta
+          name="description"
+          content="Najważniejsze informacje dla kandydatów w jednym miejscu!"
+        />
+      </Head>
+      <h1>Rekrutacja</h1>
+      <h2>Katolik Chorzów</h2>
+      <p>
+        Na tej stronie znajdą się najważniejsze informacje dla kandydatów do
+        liceum ogólnokształcącego.
+      </p>
+    </div>
+  );
+}
