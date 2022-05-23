0488abf6a1826b5a93a94af1afa618edbb4038a7
Stanisław Dera <stanislawdera@outlook.com>
2022-05-23 22:12:17 +0200

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as ga from "utils/ga";
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
-
-export default MyApp;
